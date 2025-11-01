207c63a3a1d1be7ee13ab58877187324792c213d
Mykhailo Kotov <mkhlktv@gmail.com>
2025-11-01 21:34:44 +0100

    Initial Commit

---
+++ o2scratchcardkotovm/o2scratchcardkotovmApp.swift
@@ -0,0 +1,17 @@
+//
+//  o2scratchcardkotovmApp.swift
+//  o2scratchcardkotovm
+//
+//  Created by Mykhailo Kotov on 01/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct o2scratchcardkotovmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ o2scratchcardkotovmTests/o2scratchcardkotovmTests.swift
@@ -0,0 +1,17 @@
+//
+//  o2scratchcardkotovmTests.swift
+//  o2scratchcardkotovmTests
+//
+//  Created by Mykhailo Kotov on 01/11/2025.
+//
+
+import Testing
+@testable import o2scratchcardkotovm
+
+struct o2scratchcardkotovmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
